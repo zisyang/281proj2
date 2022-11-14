021b9e734b7478c7b1b6267c27f0f5784bb6150e
zisyang <zishun.yang@sjsu.edu>
2022-11-14 01:24:29 -0800

    update

--- webapp/LT-userdata.sh
+++ webapp/LT-userdata.sh
@@ -5,6 +5,11 @@ yum install git -y
 yum install mysql -y
 systemctl start httpd
 systemctl enable httpd
+yum install docker -y
+systemctl start docker.service
+systemctl enable docker.service
+docker pull jackmleitch/whatscooking:api
+docker run --net=host -p 5000:5000 -d jackmleitch/whatscooking:api
 cd /tmp
 git clone https://github.com/zisyang/281proj2.git
 cp -a /tmp/281proj2/webapp/web/* /var/www/html/
