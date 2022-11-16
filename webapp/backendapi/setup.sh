a0b29e9af1e7205789a7c2ba47cf06954743bdc5
zisyang <zishun.yang@sjsu.edu>
2022-11-16 03:03:18 -0800

    update

--- webapp/backendapi/setup.sh
+++ webapp/backendapi/setup.sh
@@ -7,6 +7,5 @@ yum install docker -y
 systemctl start docker.service
 systemctl enable docker.service
 docker pull jackmleitch/whatscooking:api
-
 #to run the api
 docker run --net=host -p 5000:5000 -d jackmleitch/whatscooking:api
